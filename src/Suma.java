32404b3a6d38338ecb97d9256f836d151fd0869e
jsanchez <kaiserjp@gmail.com>
2018-10-29 17:57:58 +0100

    Version base suma

---
+++ src/Suma.java
@@ -0,0 +1,10 @@
+public class Suma {
+
+    public int resultat;
+    private int dato1, dato2;
+
+    int Suma(int dato1, int dato2){
+        resultat = dato1 + dato2;
+        return resultat;
+    }
+}
